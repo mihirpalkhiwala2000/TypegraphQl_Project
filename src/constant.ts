db44dcdf2482bad2456b978b6aa9e50e3cb557b2
Mihirpalkhiwala2000 <palkhiwalamihir@gmail.com>
2023-08-07 16:20:58 +0530

    Empty

--- src/constant.ts
+++
@@ -1,5 +0,0 @@
-export const errorMsgs = {
-  passwordLengthError: "password must be atleast 6 characters long",
-  incorrectPassword: "password is incorrect",
-  incorrectEmail: "No such user exists with given email ids",
-};
